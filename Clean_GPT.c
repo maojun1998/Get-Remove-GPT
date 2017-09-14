bc39ab2547e144c6fb008cb7b24ab290a00d3df0
MAOJUN <1031146367@qq.com>
2017-09-14 23:29:44 +0800

    Create Clean_GPT.c

---
+++ Clean_GPT.c
@@ -0,0 +1,43 @@
+#include<windows.h>
+#include<stdio.h>
+#define BUFF_SIZE 512																			/* the size of mbr */
+#define GTPSIZE 16896
+int main()
+
+{
+	char buffer[BUFF_SIZE]={0xAA};																/* inti the bits number is 0x00 */
+	int Write_return ;
+	int Read_return;
+	int i;
+	HANDLE  hDevice = CreateFile(																/* the hard disk handle */
+        "\\\\.\\PHYSICALDRIVE0",
+        GENERIC_WRITE,
+        FILE_SHARE_READ,
+        NULL,
+        OPEN_EXISTING,
+        NULL,
+        0);	
+	DWORD Get = BUFF_SIZE;																		/* the read the write size */
+	if (INVALID_HANDLE_VALUE ==  hDevice){
+		printf("open disk fail\n%d\n", GetLastError() );
+		MessageBox (NULL, TEXT ("You must run as admin!!!"), TEXT ("Warning"), 0) ;
+		return -1;
+	}
+	for (i = 0; i <= 32; i++ )
+	{
+		Write_return = WriteFile(
+			hDevice,
+			buffer,
+			BUFF_SIZE,
+			&Get,
+			NULL);
+	
+		if(Write_return == FALSE ){
+			printf("write fail \n%d\n", GetLastError() );
+			return -1;
+		}
+	}
+	MessageBox (NULL, TEXT ("The GPT remove successfull!Now you can reboot you computer"), TEXT ("Successful!!"), 0) ;
+	CloseHandle(hDevice );
+	return 0;
+}
